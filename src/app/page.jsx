376a3c25b82902fef89ba46d6389c641785099fe
Cláudio <claudioalvesoficial@gmail.com>
2023-12-31 07:32:32 -0300

    Update ESLint configuration and add styles to anchor tags

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,35 @@
-import Image from 'next/image'
+'use client'
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+
+  const [projectData, setProjectData] = useState();
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const username = 'claudioavgo';
+      const url = `https://api.github.com/users/${username}/repos`;
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+        console.info(data)
+
+        setProjectData(data.slice(0, 5));
+      } catch (error) {
+        console.error(`Erro ao recuperar repositórios de ${username} na API do GitHub:`, error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+  
   return (
-    <main className="">
+    <main className="flex justify-center items-center h-screen bg-neutral-900">
       <div className='space-y-5'>
         <div className='text-left'>
           <h1 className='text-2xl'>Hello, my name is <span className='font-semibold bg-pink-500 rounded px-1 text-pink-900'>Cláudio Alves</span>.</h1>
@@ -25,8 +52,8 @@ export default function Home() {
 
         <div className='text-lg font-mono text-left'>
           <h1 className='text-2xl'># My projects</h1>
-          {projectData.map((project) => (
-            <a href={`https://github.com/claudioavgo/${project?.name}`} key={project.id} target="_blank" rel="noopener noreferrer">
+          {projectData?.map((project) => (
+            <a href={`https://github.com/claudioavgo/${project?.name}`} key={project?.id} target="_blank" rel="noopener noreferrer">
               <h1>- {project.name}/{project.description?.slice(0, 30) + "..."}</h1>
             </a>
           ))}
@@ -35,14 +62,14 @@ export default function Home() {
         <div className='text-lg font-mono text-left'>
           <h1 className='text-2xl'># My contacts</h1>
           <h1>- Mail: <a href='mailto:hi@claudioav.com>'>hi@claudioav.com</a></h1>
-          <h1>- Discord: <a href='discord:claudioav>'>@claudioav</a></h1>
+          <h1>- Discord: <a href='discord:claudioav'>@claudioav</a></h1>
           <h1>- Phone: <a href='tel:+5581999416600'>+55 81 999416600</a></h1>
         </div>
 
-        <div className='text-lg font-mono text-left'>
+        {/* <div className='text-lg font-mono text-left'>
           <h1 className='text-2xl'># Posts</h1>
           <h1>- Como criar um banco de dados na azure</h1>
-        </div>
+        </div> */}
       </div>
     </main>
   )
