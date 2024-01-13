60b48e1a315af885b336d09fd034abe3b9d9f3d8
Cláudio <claudioalvesoficial@gmail.com>
2024-01-13 09:12:11 -0300

    2.1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -54,17 +54,20 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center h-full">
     
-      <TabBar/>
       
-      <div className='p-20'>
+      
+      <div className='px-5 space-y-5'>
+
+      <TabBar/>
+
       <div className='space-y-5'>
         <div className='text-left'>
-          <h1 className='text-2xl'>Hello, my name is <span className='font-semibold bg-pink-500 rounded px-1 text-pink-900'>Cláudio Alves</span>.</h1>
-          <h1 className='text-xl text-gray-300'>I'm working with Back-End development.</h1>
+          <h1 className='text-xl'>Hello, my name is <span className='font-semibold bg-pink-500 rounded px-1 text-pink-900'>Cláudio Alves</span>.</h1>
+          <h1 className='text-lg text-gray-300'>I'm working with Back-End development.</h1>
         </div>
 
         <div className='text-lg font-mono text-left'>
-          <h1 className='text-2xl'># My stacks</h1>
+          <h1 className='text-xl'># My stacks</h1>
           <h1>- Python/Flask/Django 2y</h1>
           <h1>- JavaScript/React 1y</h1>
           <h1>- Java 6m</h1>
@@ -72,13 +75,13 @@ export default function Home() {
         </div>
 
         <div className='text-lg font-mono text-left'>
-          <h1 className='text-2xl'># My experience</h1>
+          <h1 className='text-xl'># My experience</h1>
           <h1>- UNISÃOMIGUEL 1y (Now)</h1>
           <h1>- Freelance 1.5y</h1>
         </div>
 
         <div className='text-lg font-mono text-left'>
-          <h1 className='text-2xl'># My projects</h1>
+          <h1 className='text-xl'># My projects</h1>
           {projectData?.map((project) => (
             <a href={`https://github.com/claudioavgo/${project?.name}`} key={project?.id} target="_blank" rel="noopener noreferrer">
               <h1>- {project.name}/{project.description?.slice(0, 30) + "..."}</h1>
@@ -87,7 +90,7 @@ export default function Home() {
         </div>
 
         <div className='text-lg font-mono text-left'>
-          <h1 className='text-2xl'># My contacts</h1>
+          <h1 className='text-xl'># My contacts</h1>
           <h1>- Mail: <a href='mailto:hi@claudioav.com>'>hi@claudioav.com</a></h1>
           <h1>- Discord: <a href='discord:claudioav'>@claudioav</a></h1>
           <h1>- Phone: <a href='tel:+5581999416600'>+55 81 999416600</a></h1>
--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -1,11 +1,17 @@
+import Link from 'next/link'
+
 export default function TabBar() {
     return (
-        <div className="mt-5 border-white/5 shadow-sm bg-slate-900 border space-x-5 rounded flex p-5 justify-center items-center">
+        <div className="mt-5 border-white/5 shadow-sm bg-slate-900 border space-x-5 rounded flex p-5 justify-center items-center w-full md:w-auto">
             <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill="#fff" d="M12 1h2v8h8v4h-2v-2h-8V5h-2V3h2zM8 7V5h2v2zM6 9V7h2v2zm-2 2V9h2v2zm10 8v2h-2v2h-2v-8H2v-4h2v2h8v6zm2-2v2h-2v-2zm2-2v2h-2v-2zm0 0h2v-2h-2z"/>
             </svg>
-            <h1 className="text-xl underline">Home</h1>
-            <h1 className="text-xl text-white/20 select-none">Posts</h1>
+            <div className='flex justify-center items-center space-x-5'>
+                <Link href="/raio">
+                    <h1 className="text-xl text-white bg-white/10 rounded p-2">Home</h1>
+                </Link>
+                <h1 className="text-xl text-white/20 select-none">Posts</h1>
+            </div>
         </div>
     )
 }
